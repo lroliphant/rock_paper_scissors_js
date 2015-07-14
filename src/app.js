b409a88f13d90451d29262ad618a7cde8188167a
Lucy Oliphant <lucy_oliphant@yahoo.co.uk>
2015-07-14 10:08:21 +0100

    css stuff

--- src/app.js
+++ src/app.js
@@ -1,19 +1,65 @@
 var game = new RockPaperScissors();
 
-$("#rock").on("click", function() {
+// $("#rock").on("click", function() {
+//   event.preventDefault();
+//   game.computerChoice();
+//   game.playerSelection("Rock");
+//   $("#result").html(game.outcome());
+// });
+//
+// $("#paper").on("click", function() {
+//   event.preventDefault();
+//   game.computerChoice();
+//   game.playerSelection("Paper");
+//   $("#result").html(game.outcome());
+// });
+//
+// $("#scissors").on("click", function() {
+//   event.preventDefault();
+//   game.computerChoice();
+//   game.playerSelection("Scissors");
+//   $("#result").html(game.outcome());
+// });
+
+$("#rock").click(function(ev) {
+  // ev.stopPropagation();
   game.computerChoice();
-  game.playerSelection("Rock");
+  game.playerSelection('Rock');
+  $('#result').html(game.outcome());
+  console.log("yep]elfm");
+  ev.preventDefault();
+});
+
+$('#paper').click(function() {
+  event.preventDefault();
+  game.computerChoice();
+  game.playerSelection('Paper');
   $("#result").html(game.outcome());
 });
 
-$("#paper").on("click", function() {
+$('#scissors').click(function() {
+  event.preventDefault();
   game.computerChoice();
-  game.playerSelection("Paper");
-  $("#result").html(game.outcome());
+  game.playerSelection('Scissors');
+  $('#result').html(game.outcome());
 });
 
-$("#scissors").on("click", function() {
-  game.computerChoice();
-  game.playerSelection("Scissors");
-  $("#result").html(game.outcome());
-});
+// $('body').on({
+//     hover: function() {
+//         console.log("yeahhhh!!! but this doesn't work for me :(");
+//     },
+//     click: function() {
+//         console.log("kfkvmv");
+//     }
+// },'#my-button');
+
+
+// <a href="#" id="theLink">My Link</a>
+// JS
+//
+// $('theLink').click(function(ev){
+//     // do whatever you want here
+//
+//     ev.preventDefault();
+//     ev.stopPropagation();
+// });
